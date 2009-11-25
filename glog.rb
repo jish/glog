8fd69b0fc8f8f057657402f1afa7f9683b35ace4
Josh Lubaway <josh.lubaway@gmail.com>
2009-11-24 16:37:07 -0800

    Adding a simple commit list page.
    Using the glog repo itself as the repo to track initially.
    Let's hear it for recursion!

--- glog.rb
+++ glog.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
+require 'grit'
+require 'erb'
 
 get '/' do
   'Hello glog'
+end
+
+get '/commits' do
+  repo = Grit::Repo.new('.')
+  @commits = repo.commits('master', 10)
+  erb :commits
 end
