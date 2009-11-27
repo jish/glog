46936023d9c9514070313724bb99a78f3c25e2ab
Darren Boyd <darren.boyd@gmail.com>
2009-11-27 10:03:59 -0800

    Adding the basics for rspec.
    This does a very basic test, just to prove things out.
    
    You will need to install the gem 'rack-test' locally for it to work.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+
+require File.join(File.dirname(__FILE__), '..', 'glog.rb')
+
+require 'sinatra'
+require 'rack/test'
+require 'spec'
+require 'spec/autorun'
+require 'spec/interop/test'
+
+# set test environment
+set :environment, :test
+set :run, false
+set :raise_errors, true
+set :logging, false
